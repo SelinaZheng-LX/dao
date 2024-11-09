41122f24aed58f693a01f42a794b06a34a78adad
SelinaZheng <selinazhenglx@gmail.com>
2024-11-09 03:16:46 -0500

    Organized the code plus variables done

---
+++ dao/src/routes/Home.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+    return (
+        <>
+            <section className="body">
+                <div className="flex-center">
+                    <div className="title-col">
+                        <h1 className="title-text">
+                            Dao<span className="period">.</span>
+                        </h1>
+                        <p className="slogan">Personalize learning</p>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}

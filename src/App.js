ccdafded089e3b7058fc748d61f5c830b1a95053
SelinaZheng <selinazhenglx@gmail.com>
2024-11-09 01:47:05 -0500

    I give up bruh

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 function App() {
     return (
         <div className="flex h-screen w-screen items-center justify-center text-center bg-primary-color font-alata font">
-            <h1 className="text-white font-bold text-4xl">Dao</h1>
-            <span className="bg-secondary-color rounded-full h-10 w-10"></span>
+            <div class="flex flex-col justify-start">
+                <h1 class="flex items-baseline text-white font-bold text-9xl">
+                    Dao
+                    <span class="text-secondary-color inline-block text-8xl leading-none">
+                        .
+                    </span>
+                </h1>
+
+                <h2 class="flex items-start text-white text-7xl ml-7">
+                    Personalized Learning
+                </h2>
+            </div>
+            <div class="flex flex-col bg-white h-70 w-30"></div>
         </div>
     );
 }

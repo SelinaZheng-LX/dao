8eeeeb70b68d840a67c6d47c3a9c10c39b934840
SelinaZheng <selinazhenglx@gmail.com>
2024-11-09 19:34:27 -0500

    Not done with student

---
+++ dao/src/routes/studentLesson.tsx
@@ -0,0 +1,15 @@
+import Tab from "../components/TabsTeacher.tsx";
+
+//import AddBoxIcon from "@mui/icons-material/AddBox";
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+
+export default function StudentLesson() {
+    return (
+        <>
+            <section id="teacherInterface">
+                <Tab />
+            </section>
+        </>
+    );
+}

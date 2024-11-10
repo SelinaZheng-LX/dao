2ebeb80d15199e7ef256d246be3f9c0826637dd4
SelinaZheng <selinazhenglx@gmail.com>
2024-11-09 19:23:57 -0500

    vw is messed up

--- dao/src/routes/teacherLesson.tsx
+++ dao/src/routes/teacherLesson.tsx
@@ -1,50 +1,14 @@
-import LessonComp from "../components/LessonComp.tsx";
+import Tab from "../components/TabsTeacher.tsx";
+
 //import AddBoxIcon from "@mui/icons-material/AddBox";
 // import React, { useState } from "react";
 // import { Link } from "react-router-dom";
 
 export default function teacherLesson() {
-    const lessons: LessonComp[] = [
-        {
-            src: "https://picsum.photos/300",
-            alt: "Image 1",
-            subject: "Math",
-            title: "Hackathons Suck",
-            completion: false,
-        },
-        {
-            src: "https://picsum.photos/300",
-            alt: "Image 2",
-            subject: "Math",
-            title: "Hackathons Suck",
-            completion: false,
-        },
-        {
-            src: "https://picsum.photos/300",
-            alt: "Image 2",
-            subject: "Math",
-            title: "Hackathons Suck",
-            completion: false,
-        },
-        {
-            src: "https://picsum.photos/300",
-            alt: "Image 2",
-            subject: "Math",
-            title: "Hackathons Suck",
-            completion: false,
-        },
-        {
-            src: "https://picsum.photos/300",
-            alt: "Image 2",
-            subject: "Math",
-            title: "Hackathons Suck",
-            completion: false,
-        },
-    ];
     return (
         <>
             <section id="teacherInterface">
-                <LessonComp lessons={lessons} />
+                <Tab />
             </section>
         </>
     );
